f2bbae1636e21fa5c1717232c40e812d13d1cb30
kram-iv <vikram.karanth@gmail.com>
2017-06-27 01:12:41 +1000

    Initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `
+    <div id="container">
+      <div id="sidebar">
+        Sidebar will go here
+      </div>
+      <div id="content">
+        <div>
+          Article list will go here.
+        </div>
+      </div>  
+    </div>   
+             `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {

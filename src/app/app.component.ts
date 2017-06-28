a5427ddb66d72c2de711a0338e6dca602462c292
kram-iv <vikram.karanth@gmail.com>
2017-06-29 01:26:12 +1000

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
 import { Component } from '@angular/core';
 
+@Component ({
+  selector: 'app-sidebar',
+  template:`
+      <div id="sidebar">
+        Sidebar will go here
+      </div>`
+})
+export class SidebarComponent {}
+
+@Component ({
+  selector: 'app-article',
+  template:`
+      <div id="content">
+        <div>
+          Article list will go here.
+        </div>
+      </div>
+    `
+})
+export class ArticleComponent {}
+
 @Component({
   selector: 'app-root',
   //templateUrl: './app.component.html',
   template: `
     <div id="container">
-      <div id="sidebar">
-        Sidebar will go here
-      </div>
-      <div id="content">
-        <div>
-          Article list will go here.
-        </div>
-      </div>  
+      <app-sidebar></app-sidebar>
+      <app-article></app-article>
     </div>   
              `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  article: Object;
+
+  constructor() {
+    this.article = {
+      title: 'The Angular 2 screencast',
+      description: 'Easiest way to learn angular is fullstack.io'
+    }
+  }
 }
